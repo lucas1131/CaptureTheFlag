8170268ea476b158c78e02add81d72d71a196211
Lucas Soares <lucassoares1793@gmail.com>
2025-08-20 14:48:15 -0300

    Init project with First Person Template

---
+++ Source/CaptureTheFlagServer.Target.cs
@@ -0,0 +1,15 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class CaptureTheFlagServerTarget : TargetRules
+{
+	public CaptureTheFlagServerTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		ExtraModuleNames.Add("CaptureTheFlag");
+	}
+}
