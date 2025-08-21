c3b293069cfe058d0cf9d942b082a9ac4317f81e
Lucas Soares <lucassoares1793@gmail.com>
2025-08-21 13:23:38 -0300

    Create base class for animation instance so we can change HasRifle in code

---
+++ Source/CaptureTheFlag/CharacterAnimInstance.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CharacterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class CAPTURETHEFLAG_API UCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bHasRifle;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetHasRifle(const bool bInHasRifle) { bHasRifle = bInHasRifle; };
+};
