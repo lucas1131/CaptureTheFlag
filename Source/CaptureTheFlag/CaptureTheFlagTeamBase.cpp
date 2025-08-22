c879fdffd56bcc19956adac06b50faa555cd57fd
Lucas Soares <lucassoares1793@gmail.com>
2025-08-22 18:31:46 -0300

    Create class for team bases

--- Source/CaptureTheFlag/CaptureTheFlagTeamBase.cpp
+++ Source/CaptureTheFlag/CaptureTheFlagTeamBase.cpp
@@ -1,2 +1,28 @@
-﻿#include "C:\Users\lucas\Desktop\Projects\CTF\Intermediate\Build\Win64\x64\CaptureTheFlagEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.Cpp20.h"
-#include "CaptureTheFlagBase.h"
+﻿#include "CaptureTheFlagTeamBase.h"
+
+ACaptureTheFlagTeamBase::ACaptureTheFlagTeamBase(): Team(EPlayerTeam::Red) 
+{
+	Collision = CreateDefaultSubobject<USphereComponent>(FName("Collision"));
+	Collision->SetupAttachment(Mesh);
+	Collision->SetCollisionProfileName(FName("Trigger"));
+	RootComponent = Collision;
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("PPM Mesh"));
+	Mesh->CastShadow = false;
+	Mesh->SetRenderInMainPass(false);
+	Mesh->SetRenderInDepthPass(false);
+	Mesh->SetRenderCustomDepth(true);
+	Mesh->SetCollisionProfileName(FName("NoCollision"));
+	Mesh->SetCustomDepthStencilValue(Team == EPlayerTeam::Red ? 0 : 1);
+	Mesh->SetupAttachment(Collision);
+}
+
+void ACaptureTheFlagTeamBase::PostEditChangeProperty(struct FPropertyChangedEvent& Event)
+{
+	Super::PostEditChangeProperty(Event);
+
+	if (Event.GetPropertyName() == GET_MEMBER_NAME_CHECKED(ACaptureTheFlagTeamBase, Team))
+	{
+		Mesh->SetCustomDepthStencilValue(Team == EPlayerTeam::Red ? 0 : 1);
+	}
+}
--- Source/CaptureTheFlag/CaptureTheFlagTeamBase.h
+++ Source/CaptureTheFlag/CaptureTheFlagTeamBase.h
@@ -1,7 +1,25 @@
 ﻿#pragma once
+#include "CaptureTheFlagPlayerState.h"
+#include "Components/SphereComponent.h"
 
-class CaptureTheFlagBase
+#include "CaptureTheFlagTeamBase.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class ACaptureTheFlagTeamBase : public AActor
 {
-public:
+	GENERATED_BODY()
 	
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Team, meta = (AllowPrivateAccess = "true"))
+	EPlayerTeam Team;
+
+	UPROPERTY(VisibleAnywhere, Category=Collision)
+	USphereComponent* Collision;
+	UPROPERTY(VisibleAnywhere, Category=Mesh)
+	UStaticMeshComponent* Mesh;
+
+public:
+	ACaptureTheFlagTeamBase();
+
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& Event) override;
 };
