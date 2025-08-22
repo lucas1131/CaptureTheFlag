58594f9aa851ce8ac1fde335fe80f8276c263e18
Lucas Soares <lucassoares1793@gmail.com>
2025-08-22 10:57:32 -0300

    Create simple pickable flag actor that changes color based on which team picked it up

---
+++ Source/CaptureTheFlag/CaptureTheFlagFlagActor.cpp
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#include "CaptureTheFlagFlagActor.h"
+
+#include "CaptureTheFlagCharacter.h"
+#include "CaptureTheFlagPlayerState.h"
+#include "Components/SphereComponent.h"
+
+ACaptureTheFlagFlagActor::ACaptureTheFlagFlagActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Collision = CreateDefaultSubobject<USphereComponent>(TEXT("PickupCollision"));
+	Collision->InitSphereRadius(60.0f);
+	Collision->BodyInstance.SetCollisionProfileName("OverlapOnlyPawn");
+	Collision->OnComponentBeginOverlap.AddDynamic(this, &ACaptureTheFlagFlagActor::OnOverlap);
+	RootComponent = Collision;
+
+	Pole = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Pole"));
+	Pole->SetupAttachment(Collision);
+	Pole->SetCollisionProfileName(FName("NoCollision"));
+	Pole->CastShadow = false;
+
+	Flag = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Flag"));
+	Flag->SetupAttachment(Pole);
+	Flag->SetCollisionProfileName(FName("NoCollision"));
+	Flag->CastShadow = false;
+
+	FlagColors.Add(EPlayerTeam::Spectator, FColor(255, 255, 255));
+	FlagColors.Add(EPlayerTeam::Red, FColor(255, 20.4, 12.75));
+	FlagColors.Add(EPlayerTeam::Blue, FColor(20.4, 50, 255));
+
+	SetFlagColor(EPlayerTeam::Spectator);
+}
+
+void ACaptureTheFlagFlagActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FlagMaterialInstance = Flag->CreateAndSetMaterialInstanceDynamicFromMaterial(0, FlagMaterial);
+
+	StartingLocation = GetActorLocation();
+	SetFlagColor(EPlayerTeam::Spectator);
+}
+
+void ACaptureTheFlagFlagActor::SetFlagColor(const EPlayerTeam Team)
+{
+	if (FlagMaterialInstance)
+	{
+		FlagMaterialInstance->SetVectorParameterValue(FName("Color"), FlagColors[Team]);
+	}
+}
+
+void ACaptureTheFlagFlagActor::ResetFlag()
+{
+	SetActorLocation(StartingLocation);
+	SetFlagColor(EPlayerTeam::Spectator);
+}
+
+void ACaptureTheFlagFlagActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                                         int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (ACaptureTheFlagCharacter* Character = Cast<ACaptureTheFlagCharacter>(OtherActor))
+	{
+		const EPlayerTeam Team = Character->GetPlayerState<ACaptureTheFlagPlayerState>()->GetTeam();
+		Character->GrabFlag(this);
+		SetFlagColor(Team);
+	}
+}
---
+++ Source/CaptureTheFlag/CaptureTheFlagFlagActor.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CaptureTheFlagFlagActor.generated.h"
+
+enum class EPlayerTeam : uint8;
+class USphereComponent;
+
+UCLASS(Blueprintable, BlueprintType)
+class CAPTURETHEFLAG_API ACaptureTheFlagFlagActor : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Collision)
+	USphereComponent* Collision;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Mesh)
+	UStaticMeshComponent* Pole;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Mesh)
+	UStaticMeshComponent* Flag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Materials)
+	TMap<EPlayerTeam, FColor> FlagColors;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Materials)
+	UMaterialInterface* FlagMaterial;
+
+private:
+	UPROPERTY()
+	class UMaterialInstanceDynamic* FlagMaterialInstance;
+	FVector StartingLocation;
+
+public:
+	ACaptureTheFlagFlagActor();
+	virtual void BeginPlay() override;
+	void SetFlagColor(EPlayerTeam Team);
+	void ResetFlag();
+
+private:
+	UFUNCTION()
+	void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
