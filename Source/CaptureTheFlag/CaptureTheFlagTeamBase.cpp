c72ce048787c8149764166797411132d08688eb8
Lucas Soares <lucassoares1793@gmail.com>
2025-08-22 18:20:03 -0300

    Fix log error with MF call saying there was no input with given names, just recreated the node and rewire inputs

---
+++ Source/CaptureTheFlag/CaptureTheFlagTeamBase.cpp
@@ -0,0 +1,2 @@
+﻿#include "C:\Users\lucas\Desktop\Projects\CTF\Intermediate\Build\Win64\x64\CaptureTheFlagEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.Cpp20.h"
+#include "CaptureTheFlagBase.h"
