469fc4aaf95cbad2f397c9e7ae2d5f5f347155a7
Lucas Soares <lucassoares1793@gmail.com>
2025-08-24 17:22:57 -0300

    Create ability for weapon firing

---
+++ Source/Abilities/Private/FireWeaponAbility.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FireWeaponAbility.h"
+
+#include "CaptureTheFlag/CaptureTheFlagCharacter.h"
+
+UFireWeaponAbility::UFireWeaponAbility()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	ActivationOwnedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Character.Weapon.FireSemiAuto")));
+}
+
+bool UFireWeaponAbility::CanActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                            const FGameplayAbilityActorInfo* ActorInfo,
+                                            const FGameplayTagContainer* SourceTags,
+                                            const FGameplayTagContainer* TargetTags,
+                                            FGameplayTagContainer* OptionalRelevantTags) const
+{
+	if (!Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags))
+	{
+		return false;
+	}
+
+	if (const ACaptureTheFlagCharacter* Character = Cast<ACaptureTheFlagCharacter>(ActorInfo->AvatarActor))
+	{
+		return Character->HasWeaponEquipped();
+	}
+
+	return false;
+}
+
+void UFireWeaponAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                         const FGameplayAbilityActorInfo* ActorInfo,
+                                         const FGameplayAbilityActivationInfo ActivationInfo,
+                                         const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (ACaptureTheFlagCharacter* Character = Cast<ACaptureTheFlagCharacter>(ActorInfo->AvatarActor))
+	{
+		Character->FireWeapon();
+	}
+
+	// End immediately, no automatic fire for now
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+}
---
+++ Source/Abilities/Private/FireWeaponAbility.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+
+#include "FireWeaponAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABILITIES_API UFireWeaponAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	UFireWeaponAbility();
+
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	                                const FGameplayAbilityActorInfo* ActorInfo,
+	                                const FGameplayTagContainer* SourceTags = nullptr,
+	                                const FGameplayTagContainer* TargetTags = nullptr,
+	                                FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	                             const FGameplayAbilityActorInfo* ActorInfo,
+	                             const FGameplayAbilityActivationInfo ActivationInfo,
+	                             const FGameplayEventData* TriggerEventData) override;
+};
