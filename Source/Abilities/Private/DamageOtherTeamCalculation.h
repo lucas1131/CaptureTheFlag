1769ef3e29b30c4e36b31fd902ceb2a9c2c1ac23
Lucas Soares <lucassoares1793@gmail.com>
2025-08-25 19:17:04 -0300

    Reposition arms a little bit more

---
+++ Source/Abilities/Private/DamageOtherTeamCalculation.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "DamageOtherTeamCalculation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABILITIES_API UDamageOtherTeamCalculation : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+};
