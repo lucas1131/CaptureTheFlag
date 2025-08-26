349ff45a78ddfd0f1d879f0466ab8706c2fc1cc5
Lucas Soares <lucassoares1793@gmail.com>
2025-08-26 15:39:10 -0300

    Add UI dependency to abilities module for GameplayCues to set ui parameters

--- Source/Abilities/Abilities.Build.cs
+++ Source/Abilities/Abilities.Build.cs
@@ -23,7 +23,8 @@ public class Abilities : ModuleRules
                 "GameplayAbilities", 
                 "GameplayTags", 
                 "GameplayTasks", 
-                "CaptureTheFlag"
+                "CaptureTheFlag",
+                "UI"
             }
         );
     }
