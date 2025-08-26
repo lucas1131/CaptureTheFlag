6c5a5a060d394729970ab87783e69eecd18df4b9
Lucas Soares <lucassoares1793@gmail.com>
2025-08-26 15:35:08 -0300

    Remove CaptureTheFlag module from UI dependency, i dont know why it was added there, it shouldnt be

--- Source/UI/UI.Build.cs
+++ Source/UI/UI.Build.cs
@@ -10,7 +10,6 @@ public class UI : ModuleRules
             new string[]
             {
                 "Core",
-                "CaptureTheFlag",
                 "UMG", 
             }
         );
