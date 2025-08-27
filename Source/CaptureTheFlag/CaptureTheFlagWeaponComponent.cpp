e46664928fae69a3cc9c6dc8464bfea5e06c11bc
Lucas Soares <lucassoares1793@gmail.com>
2025-08-26 23:57:54 -0300

    Guard projectile creation, sometimes it spawns as null, I think it is spawned already hitting someting and is immediately destroyed

--- Source/CaptureTheFlag/CaptureTheFlagWeaponComponent.cpp
+++ Source/CaptureTheFlag/CaptureTheFlagWeaponComponent.cpp
@@ -73,15 +73,18 @@ void UCaptureTheFlagWeaponComponent::Fire() const
 	
 		// Spawn the projectile at the muzzle
 		ACaptureTheFlagProjectile* Projectile = World->SpawnActor<ACaptureTheFlagProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
-		Projectile->SetInstigator(Character);
-		if (IsValid(HitEffectClass))
+		if (Projectile)
 		{
-			Projectile->SetHitEffect(HitEffectClass);
-		}
-		else
-		{
-			UE_LOG(LogTemp, Warning, TEXT("Weapon trying to set null HitEffect to projectile"));
-			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Weapon trying to set null HitEffect to projectile"));
+			Projectile->SetInstigator(Character);
+			if (IsValid(HitEffectClass))
+			{
+				Projectile->SetHitEffect(HitEffectClass);
+			}
+			else
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Weapon trying to set null HitEffect to projectile"));
+				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Weapon trying to set null HitEffect to projectile"));
+			}
 		}
 	}
 }
